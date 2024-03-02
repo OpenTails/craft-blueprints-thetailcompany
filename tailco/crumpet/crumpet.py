0933dc35ede527e10b7cc016e0ce8b90a7ce26c0
Dan Leinir Turthra Jensen <admin@leinir.dk>
2024-03-02 11:39:21 +0000

    No such module in qt6

--- tailco/crumpet/crumpet.py
+++ tailco/crumpet/crumpet.py
@@ -53,7 +53,6 @@ class subinfo(info.infoclass):
         # build using Craft)
         self.runtimeDependencies["libs/qt6/qtconnectivity"] = "default" # For QtBluetooth
         self.runtimeDependencies["libs/qt6/qtmultimedia"] = "default"
-        self.runtimeDependencies["libs/qt6/qtquickcontrols"] = "default" # For the file picker dialog
         self.runtimeDependencies["libs/qt6/qtquickcontrols2"] = "default"
         self.runtimeDependencies["libs/qt6/qtremoteobjects"] = "default"
         self.runtimeDependencies["libs/qt6/qtsensors"] = "default"
